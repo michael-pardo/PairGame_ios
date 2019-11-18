36c272c272113a8d7c8dd150b3429a7bde232cbf
michael Parod <michael.pardo@unillanos.edu.co>
2019-11-17 23:40:39 -0500

    Creating Login Screen

---
+++ pairgame/pairgame/controller/LoginVC.swift
@@ -0,0 +1,30 @@
+//
+//  LoginVC.swift
+//  pairgame
+//
+//  Created by Michael Pardo  on 17/11/19.
+//  Copyright © 2019 Michael Pardo . All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
